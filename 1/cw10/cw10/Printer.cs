6ea77a4e75ca80b6af712cf99a6bbfe6f19cffde
sh-zol <zolghadrisahin@ymail.com>
2023-12-01 10:09:14 +0330

    Add project files.

---
+++ 1/cw10/cw10/Printer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw10
+{
+    public class Printer
+    {
+        public void PrintWelcome()
+        {
+            Console.WriteLine("Welcome");
+        }
+    }
+}
---
+++ 1/cw10/cw10/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace cw10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printer = new Printer();
+            printer.PrintWelcome();
+        }
+    }
+}
